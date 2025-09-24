0fc9c8ebd9eb7b56916d2b027f023a9a50360df6
dmthompson1224-cloud <dmthompson1224@gmail.com>
2025-09-24 14:57:31 -0400

    inital commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args){
+        int NUM_TO_EXAMINE = 2;
+
+        if (NUM_TO_EXAMINE %2 == 0) {
+            System.out.println(NUM_TO_EXAMINE + " Even."); }
+        else {
+            System.out.println(NUM_TO_EXAMINE + " Odd."); }
+
+
+    }
+}
